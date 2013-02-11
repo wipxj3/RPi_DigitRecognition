6923a68297b7dc0b3281f25809f704e100def580
wipxj3 <vladimir.pintea@gmail.com>
2013-02-11 14:42:58 +0200

    added files to RPi_recognition Set main capture functionality.

--- RPi_Led.py
+++ RPi_Led.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 __author__ = 'dexter'
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT)
+
+
+def ledOn():
+    GPIO.output(8, False) # LED OFF
+    time.sleep(1)
+    GPIO.output(8, True) # LED ON
+    return 1
+
+def ledOff():
+    GPIO.output(8, True) # LED OFF
+    time.sleep(1)
+    GPIO.output(8, False) # LED ON
+    return 1
+'''
+if __name__ == '__main__':
+    print 'Turning LED ON'
+    ledOn()
+    print 'Turning LED OFF'
+    ledOff()
+'''
--- RPi_Main.py
+++ RPi_Main.py
@@ -1,1 +1,18 @@
 __author__ = 'dexter'
+import RPi_Camera, RPi_Led
+
+if __name__ == '__main__':
+    while True:
+        try:
+            print 'Turning LED ON'
+            RPi_Led.ledOn()
+
+            print 'Starting capture...'
+            capture = RPi_Camera.CaptureImage()
+            image = capture.getImageIndex()
+            print 'Saved as ' + image + '.png'
+
+            print 'Turning LED OFF'
+            RPi_Led.ledOff()
+        except:
+            print 'Capture device not available'
